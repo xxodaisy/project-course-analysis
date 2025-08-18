06b66d508515f7130daa03e00e8fc998366c0c45
Fathia <main.nurfathia@gmail.com>
2025-08-18 14:54:57 +0700

    add test connect db

---
+++ tes_connect.py
@@ -0,0 +1,17 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+try:
+    conn = psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
+    print("Connection success!")
+    conn.close()
+except Exception as e:
+    print("Connection failed:", e)
